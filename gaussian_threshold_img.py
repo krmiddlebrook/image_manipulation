702705e4bec2adf8263585b31d1249708e7b6048
Kai Middlebrook <kaimiddlebrook@Kais-MacBook-Air.local>
2017-12-28 17:37:20 -0800

    added cv2 and filepath search

--- gaussian_threshold_img.py
+++ gaussian_threshold_img.py
@@ -1,20 +1,27 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-
-image   = cv2.imread('images/kai_matt.jpg', 0)    # read in image - convert to grayscale
-image   = cv2.bilateralFilter(image, 9, 75, 75)   # bilaterally filters the image to reduce noise, blur, and maintain edges
-
-gauss_thresh = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,11,2)     # finds threshold of img using gaussian nearest window technique
-
-titles   = [str(image), 'Gaussian Thresholding']
-images  = [image, gauss_thresh]
+import os
 
 
-for i in range(2):
-    plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray')
-    plt.title(titles[i])
-    plt.xticks([]), plt.yticks([])
-plt.show()
+path    = input("Enter the name of your image file: ")
+path    = os.path.abspath(str(path))
+print(path)
+
+# image   = cv2.imread(str(path), 0)    # read in image - convert to grayscale
+# plt.show()
+# image   = cv2.bilateralFilter(image, 9, 75, 75)   # bilaterally filters the image to reduce noise, blur, and maintain edges
+#
+# gauss_thresh = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+#             cv2.THRESH_BINARY,11,2)     # finds threshold of img using gaussian nearest window technique
+#
+# titles   = [str(image), 'Gaussian Thresholding']
+# images  = [image, gauss_thresh]
+#
+#
+# for i in range(2):
+#     plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray')
+#     plt.title(titles[i])
+#     plt.xticks([]), plt.yticks([])
+# plt.show()
 
